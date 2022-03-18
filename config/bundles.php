d47fbe6415fe782a3bfd36eae919fd0523455d6b
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-18 14:29:06 +0100

    EIT-47 Add EasyAdmin dashboard for Admin

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
