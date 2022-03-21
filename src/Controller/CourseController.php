a7a21146c6d171e2009a80199e9dd147f46686c1
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-21 16:46:51 +0100

    EIT-73 Add list of all courses

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class CourseController extends AbstractController
 {
     #[Route('/formations', name: 'app_courses')]
-    public function index(): Response
+    public function index(CourseRepository $courseRepository): Response
     {
         return $this->render('course/index.html.twig', [
-            'controller_name' => 'CourseController',
+            'courses' => $courseRepository->findAll(),
         ]);
     }
 }
