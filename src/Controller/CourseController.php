adc200021396565b29562d09244b691f2ec2713c
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-21 17:14:23 +0100

    EIT-75 Add contents to course detail

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\CourseRepository;
+use App\Repository\LessonRepository;
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,16 +14,20 @@ class CourseController extends AbstractController
     #[Route('/formations', name: 'app_courses')]
     public function index(CourseRepository $courseRepository): Response
     {
+        $courses = $courseRepository->findAll();
+
         return $this->render('course/index.html.twig', [
-            'courses' => $courseRepository->findAll(),
+            'courses' => $courses,
         ]);
     }
 
     #[Route('/formations/{id}', name: 'app_course_detail')]
-    public function courseDetail(int $id, CourseRepository $courseRepository): Response
+    public function courseDetail(int $id, CourseRepository $courseRepository, SectionRepository $sectionRepository, LessonRepository $lessonRepository): Response
     {
+        $course = $courseRepository->findOneBy(['id' => $id]);
+
         return $this->render('course/course_detail.html.twig', [
-            'course' => $courseRepository->findOneBy(['id' => $id]),
+            'course' => $course,
         ]);
     }
 }
