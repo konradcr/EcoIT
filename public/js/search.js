3a555fc6b14b78f05f20e1a9acf6ee5d37305e53
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-23 18:22:20 +0100

    EIT-91 Add script for search on courses

---
+++ public/js/search.js
@@ -0,0 +1,24 @@
+function searchCourse() {
+    // Declare variables
+    var search, filter, list, cards, course, i, txtValue;
+
+    // Get search text
+    search = document.getElementById('mySearch');
+    filter = search.value.toUpperCase();
+    // Get list of courses
+    list = document.getElementById("courseList");
+    // Get courses
+    cards = list.getElementsByClassName("card");
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < cards.length; i++) {
+        // Get course name
+        course = cards[i].getElementsByClassName("card-title")[0];
+        txtValue = course.textContent || course.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
