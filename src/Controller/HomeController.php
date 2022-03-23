2124adf8746043efde81394772794fcbcd74158c
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-23 10:59:56 +0100

    EIT-87 Edit home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CourseRepository $courseRepository): Response
     {
+        $last3courses = $courseRepository->findBy([], ['creationDate' => 'DESC'], 3);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'lastCourses' => $last3courses
         ]);
     }
 }
