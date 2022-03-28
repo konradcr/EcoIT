335b814c43bf9b9c06d6b583b9b2e74557b9603f
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-28 10:22:43 +0200

    EIT-116 Add EditTeacherFormType

---
+++ src/Form/EditTeacherFormType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Teacher;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class EditTeacherFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('profilePicture', FileType::class, [
+                'label' => 'Photo de profil',
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new Image([
+                    ])
+                ],
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => "Prénom",
+                'attr' => ['placeholder' => 'Prénom'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Saisissez un prénom.',
+                    ]),
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => "Nom",
+                'attr' => ['placeholder' => 'Nom'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Saisissez un nom.',
+                    ]),
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => "Description",
+                'attr' => ['placeholder' => 'Détaillez vos compétences, vos spécialités, vos motivations...'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Saisissez une description.',
+                    ]),
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Teacher::class,
+        ]);
+    }
+}
